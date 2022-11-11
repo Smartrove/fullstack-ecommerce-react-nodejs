a46ffd7cf1b470235150f4368f39c5889fc4a0e3
Lateef Wasiu Olalekan <olaokunolalekan@gmail.com>
2022-11-11 23:36:02 +0100

    updating code

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -16,13 +16,65 @@ const Top = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 20px;
 `;
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
-const Bottom = styled.div``;
+const TopTexts = styled.div``;
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Info = styled.div`
+  flex: 3;
+`;
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+const Image = styled.img`
+  width: 300px;
+`;
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const ProductName = styled.span`
+  flex: 1;
+`;
+const ProductID = styled.span`
+  flex: 1;
+`;
+const ProductColor = styled.div`
+  flex: 1;
+`;
+const ProductSize = styled.span`
+  flex: 1;
+`;
+const PriceDetail = styled.span`
+  flex: 1;
+`;
+const Summary = styled.div``;
+
 const Cart = () => {
   return (
     <Container>
@@ -32,9 +84,35 @@ const Cart = () => {
         <Title>Your Bag</Title>
         <Top>
           <TopButton>Continue Shopping</TopButton>
-          <TopButton>Checkout Now</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist(0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">Checkout Now</TopButton>
         </Top>
-        <Bottom></Bottom>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="product-3.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b> Swaggie Wear
+                  </ProductName>
+                  <ProductID>
+                    <b>ProductID: </b> 23456
+                  </ProductID>
+                  <ProductColor />
+                  <ProductSize>
+                    <b>ProductSize: </b> 40
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>Price</PriceDetail>
+            </Product>
+          </Info>
+          <Summary>Summary</Summary>
+        </Bottom>
       </Wrapper>
       <Footer />
     </Container>
